9fec5cab20240f2e1a88444640a90f56a104e7f9
Andrzej Dulny <andzej.dulny@gmail.com>
2025-04-10 15:57:53 +0200

    Make scipy optimizer default

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.costs.base import BaseCost
-from src.optimizers import OptunaOptimizer
+from src.optimizers import OptunaOptimizer, ScipyOptimizer
 from src.data import Simulation, CoilConfig
 
 import numpy as np
@@ -15,6 +15,6 @@ def run(simulation: Simulation,
             cost_function: Cost function object
             timeout: Time (in seconds) after which the evaluation script will be terminated
     """
-    optimizer = OptunaOptimizer(cost_function=cost_function, max_iter=1000)
+    optimizer = ScipyOptimizer(cost_function=cost_function, max_iter=200)
     best_coil_config = optimizer.optimize(simulation)
     return best_coil_config
